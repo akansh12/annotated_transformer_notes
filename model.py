1eb5bd025dd6adda4db1749d95a5a985b87a9d1f
akansh12 <akanshmaurya@gmail.com>
2025-01-16 00:07:58 +0100

    ViT started

--- model.py
+++ model.py
@@ -1,14 +1,46 @@
 import torch
 import torch.nn as nn
+import math
 
 class InputEmbedding(nn.Module):
     
-    def __init__(self, d_input: int, vocab_size: int):
+    def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
-        self.d_model = d_input
-        self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, d_input)
+        self.d_model = d_model  # 512
+        self.vocab_size = vocab_size # How many words in the vocabulary
+        self.embedding = nn.Embedding(vocab_size, d_model) 
 
     def forward(self, x):
-        return self.embedding(x)
+        return self.embedding(x) * math.sqrt(self.d_model)  #TODO:Is x here string? or tensor?
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model # 512
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) 
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        #apply sin to even indices
+        pe[:, 0::2] = torch.sin(position * div_term)
+        #apply cos to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        pe = pe.unsqueeze(0) 
+
+        self.register_buffer('pe', pe) # Register the positional encoding as a buffer so it will be saved with the model
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+
+    
+
+
+
         
